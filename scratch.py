5686cb56f26be15e0436cadc2db94ae59281f4cd
ABHINAV GHOSH <35833124+logicinfinite@users.noreply.github.com>
2019-03-21 11:33:42 +0530

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,8 +1,9 @@
 from PIL import Image
 
-i=Image.open("C:\\Users\\KIIT\\N.jpg").convert("RGBA")
+i=Image.open("FILE.jpg").convert("RGBA")
+#CONVERTING TO RGBA IS OPTIONAL ,BUT IS RECOMMENDED TO AVOID MASKING ERRORS WITH LARGE FILES
 bg=Image.new("RGBA",(20,220),(255,255,255))
 x,y=i.size
 bg.paste(i, (0,0,x,y), i)
 i = i.convert("RGB")
-bg.save("g.png",quality=95)
+bg.save("FILE.png",quality=95)
